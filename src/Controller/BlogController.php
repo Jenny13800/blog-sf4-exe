9b7e141e6cdc206a10bc051d1f4f7016ac3055e8
Jennifer <jcour@montmirail.com>
2018-08-17 16:18:06 +0200

    Modify views and BlogController

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +12,14 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index()
+    public function index(ArticleRepository $repo)
     {
+        //plus besoin grace à l'injection de dépendance
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
         return $this->render('blog/index.html.twig', [
             'controller_name' => 'BlogController',
+            'articles' => $articles
         ]);
     }
 
@@ -24,12 +30,25 @@ class BlogController extends AbstractController
     public function home(){
         return $this->render('blog/home.html.twig');
     }
+    
+    /*public function show(ArticleRepository $repo,$id){
+        //$repo = $this->getDoctrine()->getRepository(Article::class);
+
+        $article = $repo->find($id);
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }*/
 
     /**
-     * @Route("/blog/12", name="blog_show")
+     * @Route("/blog/{id}", name="blog_show")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function show(){
-        return $this->render('blog/show.html.twig');
+    public function show(Article $article){
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
     }
 }
