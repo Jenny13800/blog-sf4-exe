2586d79774c374f9c706dc1f1f38d40ea008f05e
Jennifer <jcour@montmirail.com>
2018-08-20 16:26:13 +0200

    add library faker + modify form

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,16 +12,58 @@ class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // create 10 posts
-        for($i = 1; $i <= 10; $i++) {
-            $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setContent("<p>Contenu de l'article n°$i</p>")
-                    ->setImage("http://placehold.it/350x150")
-                    ->setCreatedAt(new \DateTime());
-            $manager->persist($article);
-        }
+        $faker = \Faker\Factory::create('fr_FR');
 
+        // 3 catégories fake
+        for($i = 1; $i <= 3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            // créer entre 4 et 6 articles
+            // mt_rand = nb au hasard
+            for($j = 1; $j <= mt_rand(4, 6); $j++) {
+                $article = new Article();
+
+                // setContent attend chaîne de caractère
+                // $faker->paragraphs() => retourne un tableau
+                // rejoindre la fin du tableau avec une balise p fermante et ouverte
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+
+                $article->setTitle($faker->sentence())
+                    ->setContent($content)
+                    ->setImage($faker->imageUrl())
+                    ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                    ->setCategory($category);
+
+                $manager->persist($article);
+
+                // on donne des commentaires à l'articles
+                for($k = 1; $k <= mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+
+                    // date d'aujourd'hui
+                    //$now = new \DateTime();
+                    // interval entre aujourd'hui et la date de création de l'article
+                    /*$interval = $now->diff($article->getCreatedAt());
+                    $days = $interval->days;
+                    $minimum = '-' . $days . ' days'; // par ex : -100 days*/
+
+                    $days = (new \DateTime())->diff($article->getCreatedAt())->days;
+
+                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween('-' . $days . ' days'))
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
+        }
         $manager->flush();
     }
 }
